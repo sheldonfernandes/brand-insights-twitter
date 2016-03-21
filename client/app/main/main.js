a7a9d92c8f84bf9e34cc640222d9c7d617a5ebd2
unknown <sheldon_fernandes@GOL05195.persistent.co.in>
2016-03-21 12:17:36 +0530

    initial commit  - scaffolded yeoman boilerplace with generator-angular-fullstack

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('d3dashApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        template: '<main></main>'
+      });
+  });
